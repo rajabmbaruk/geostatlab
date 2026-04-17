99245825d4dcd2765f6fe26721a7241e1e88f768
rajabmbaruk <rmbaruku@knbs.or.ke>
2026-04-17 09:33:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -160,19 +160,17 @@ elif module == "📈 Data Analysis":
   #  selected = st.session_state.selected_county
    # st.subheader(f"Analysis for {selected}")
 
- 
-
-  selected = st.session_state.selected_county
-  county_data = df[df["County"] == selected]
+selected = st.session_state.selected_county
+county_data = df[df["County"] == selected]
   
-  csv = county_data.to_csv(index=False).encode("utf-8")
+csv = county_data.to_csv(index=False).encode("utf-8")
   
-  st.download_button(
-      label="📥 Download Selected County Data",
-      data=csv,
-      file_name=f"{selected}_data.csv",
-      mime="text/csv"
-  )
+st.download_button(
+    label="📥 Download Selected County Data",
+    data=csv,
+    file_name=f"{selected}_data.csv",
+    mime="text/csv"
+ )
     indicator_map = {
     "Household Income (KES)": "Household_Income",
         "Poverty Rate (%)": "Poverty_Rate",
