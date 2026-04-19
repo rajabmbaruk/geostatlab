04020cb944dce66efc4d9eaf48f5cc0e4a1e0b25
rajabmbaruk <rmbaruku@knbs.or.ke>
2026-04-19 14:35:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -289,14 +289,24 @@ with tab4:
   county_list = df["County"].tolist()
 
     # Ensure valid selection
-  if st.session_state.selected_county not in county_list:
+  #county_list = df["County"].tolist()
+
+  selected = st.session_state.get("selected_county", county_list[0])
+
+  # Ensure it's valid
+  if selected not in county_list:
+    selected = county_list[0]
+    st.session_state.selected_county = selectedif st.session_state.selected_county not in county_list:
         st.session_state.selected_county = county_list[0]
 
   selected = st.selectbox(
-        "Select County",
-        county_list,
-        key="selected_county"
+    "Select County for Details",
+    county_list,
+    index=county_list.index(selected)
   )
+
+  # Sync
+  st.session_state.selected_county = selected
 
   county_data = df[df["County"] == selected]
 
