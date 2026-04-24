676cb8a96c1532dc70340aa0d7c59b78e52fafd7
rajabmbaruk <rmbaruku@knbs.or.ke>
2026-04-24 16:39:32 +0300

    Update state.py

--- core/state.py
+++ core/state.py
@@ -1,17 +1,21 @@
 import streamlit as st
 
-def init_state():
+def init_state(years):
     defaults = {
-        "year": 2024,
-        "active_tab": "Home",
+        "year": max(years),
+        "active_tab": 0,
         "playing": False,
+        "selected_county": None,
         "show_onboarding": True,
         "onboarding_step": 0,
         "presentation_mode": False,
         "slide_index": 0,
         "global_indicator": "Household_Income",
+        "role": "Analyst"
     }
 
     for k, v in defaults.items():
         if k not in st.session_state:
             st.session_state[k] = v
+
+
