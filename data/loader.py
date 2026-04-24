e400b01cdc269e8a768553bb74be08c7704d45b5
rajabmbaruk <rmbaruku@knbs.or.ke>
2026-04-24 10:55:30 +0300

    Add files via upload

--- data/loader.py
+++ data/loader.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import json
+import os
 
+def load_data():
+    return pd.read_csv("geostatlab_data.csv")
+
+def load_geojson():
+    path = os.path.join("assets", "kenya_counties.geojson")
+    with open(path) as f:
+        return json.load(f)
