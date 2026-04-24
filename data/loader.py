1be38cfebe8adb6fedcedf8ec5569826352b42b2
rajabmbaruk <rmbaruku@knbs.or.ke>
2026-04-24 11:39:39 +0300

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -4,7 +4,17 @@ import json
 import streamlit as st
 
 def load_data():
-    path = os.path.join(os.path.dirname(__file__), "geostatlab_data.csv")
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # go one level up from /data/
+    root_dir = os.path.abspath(os.path.join(base_dir, ".."))
+
+    path = os.path.join(root_dir, "geostatlab_data.csv")
+
+    if not os.path.exists(path):
+        st.error(f"Dataset not found at: {path}")
+        return pd.DataFrame()
+
     return pd.read_csv(path)
 
 def load_geojson():
