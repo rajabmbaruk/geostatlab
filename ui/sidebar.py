ede705621166fcf45f6db05ef6984fe3ae0c84ca
rajabmbaruk <rmbaruku@knbs.or.ke>
2026-04-24 20:41:57 +0300

    Update sidebar.py

--- ui/sidebar.py
+++ ui/sidebar.py
@@ -1,11 +1,27 @@
-# ui/sidebar.py
 import streamlit as st
+from core.state import set_page
 
 def sidebar_nav():
-    st.sidebar.title("🌍 GeoStatLab")
 
-    return st.sidebar.radio(
-        "Navigation",
-        ["Home", "Dataset", "Survey", "Maps", "Analysis", "Policy", "Quiz"],
-        key="nav_main"
-    )
+    with st.sidebar:
+        st.title("🌍 GeoStatLab")
+
+        page = st.radio(
+            "Navigate",
+            ["Home", "Dataset", "Survey", "Maps", "Analysis", "Policy", "Quiz"],
+            key="nav_radio"
+        )
+
+        role = st.selectbox(
+            "Role",
+            ["Analyst", "Policy Maker"],
+            key="role_select"
+        )
+
+    st.session_state.role = role
+    set_page(page)
+
+    return page
+
+
+
