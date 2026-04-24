9442adc62ee69e84252db24d5af6c6926d5bb9bc
rajabmbaruk <rmbaruku@knbs.or.ke>
2026-04-24 12:10:25 +0300

    Update speech.py

--- services/speech.py
+++ services/speech.py
@@ -1,1 +1,8 @@
-
+def speak(text):
+    try:
+        import pyttsx3
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
+    except Exception:
+        pass  # fail silently in cloud
