54aad178efe47ba20f4c672d37e30587a592fecc
rajabmbaruk <rmbaruku@knbs.or.ke>
2026-04-24 11:46:45 +0300

    Update home.py

--- ui/home.py
+++ ui/home.py
@@ -1,3 +1,11 @@
 def show_home():
     import streamlit as st
     st.title("Home")
+    
+import streamlit as st
+
+def show_home(df):
+    st.title("🌍 GeoStatLab Home")
+
+    st.write("Dataset overview:")
+    st.dataframe(df.head())
